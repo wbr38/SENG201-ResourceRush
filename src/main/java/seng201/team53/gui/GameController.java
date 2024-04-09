e80a634433f2339504d69a8b0e07d67a5b2ec4e2
chi125 <chi125@uclive.ac.nz>
2024-04-09 15:44:37 +1200

    Prevent beginning the place tower interaction if the current interation is not none

--- src/main/java/seng201/team53/gui/GameController.java
+++ src/main/java/seng201/team53/gui/GameController.java
@@ -7,7 +7,6 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import seng201.team53.App;
 import seng201.team53.game.map.Map;
@@ -57,6 +56,8 @@ public class GameController {
     @FXML
     void onCreateWoodTowerBtnClick(MouseEvent event) {
         Map map = App.getApp().getGameEnvironment().getRound().getMap();
+        if (map.getCurrentInteraction() != MapInteraction.NONE) // prevents starting the "placing" methods running again
+            return;
         LumberMillTower tower = new LumberMillTower();
         map.startPlacingTower(tower);
     }
