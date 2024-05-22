66d7f2deda5c877d400f0f882843464dec89774e
Corey Hines <chi125@uclive.ac.nz>
2024-05-22 15:42:59 +1200

    completed javadocs for GameRoundFactory

--- src/main/java/seng201/team53/game/round/GameRoundFactory.java
+++ src/main/java/seng201/team53/game/round/GameRoundFactory.java
@@ -5,13 +5,22 @@ import seng201.team53.game.GameDifficulty;
 import seng201.team53.game.GameEnvironment;
 import seng201.team53.items.ResourceType;
 
+/**
+ * This class is responsible for creating instances of the GameRound class
+ * It uses the games difficulty and round number to determine the number of carts and velocities
+ */
 public class GameRoundFactory {
-    public GameRound getRound(int roundNumber) {
 
+    /**
+     * Creates a new GameRound instance
+     * The properties of the round are determined based on the games difficulty and round number
+     * @param roundNumber The number of the round
+     * @return A new GameRound instance
+     */
+    public GameRound getRound(int roundNumber) {
         GameDifficulty difficulty = GameEnvironment.getGameEnvironment().getDifficulty();
         GameRound round = new GameRound(roundNumber);
-        
-        // Add carts
+
         float cartVelocity = calculateCartVelocity(difficulty, roundNumber);
         int numCarts = difficulty.getNumberOfCarts(roundNumber);
         for (int i = 0; i < numCarts; i++) {
@@ -28,6 +37,13 @@ public class GameRoundFactory {
         return round;
     }
 
+    /**
+     * Calculates the velocity of the carts in a round
+     * The velocity is determined based on the games difficulty and round number
+     * @param difficulty The games difficulty
+     * @param roundNumber The number of the round
+     * @return The calculated cart velocity
+     */
     private float calculateCartVelocity(GameDifficulty difficulty, int roundNumber) {
         float minVelocity = 5;
         float roundIncrease = 1.5f * (float)Math.log(roundNumber);
