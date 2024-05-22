62dc4c88161925540a0c49ae59361182d9882429
Corey Hines <chi125@uclive.ac.nz>
2024-05-22 15:44:24 +1200

    completed javadocs for CartState

--- src/main/java/seng201/team53/game/state/CartState.java
+++ src/main/java/seng201/team53/game/state/CartState.java
@@ -1,7 +1,22 @@
 package seng201.team53.game.state;
 
+/**
+ * An enum representing the various state a cart can be in
+ */
 public enum CartState {
+
+    /**
+     * The cart is waiting to be spawned
+     */
     WAITING,
+
+    /**
+     * The cart is currently traversing the path
+     */
     TRAVERSING_PATH,
+
+    /**
+     * The cart has completed the path
+     */
     COMPLETE_PATH
 }
