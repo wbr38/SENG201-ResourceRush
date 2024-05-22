243e6e79468caa0c6ff9c11846a1d03c6a2ad5a8
Corey Hines <chi125@uclive.ac.nz>
2024-05-22 15:53:29 +1200

    completed javadocs for GameDifficulty

--- src/main/java/seng201/team53/game/GameDifficulty.java
+++ src/main/java/seng201/team53/game/GameDifficulty.java
@@ -1,6 +1,14 @@
 package seng201.team53.game;
 
+/**
+ * An enum representing the difficulty of the game
+ * Each difficulty has a set of parameters that affect the game play, such as the starting money, the number of carts,
+ * and the money earn multiplier.
+ */
 public enum GameDifficulty {
+    /**
+     * Represents the easy difficulty level
+     */
     EASY(
          "Easy",
          600,
@@ -11,6 +19,9 @@ public enum GameDifficulty {
          0.7f,
          0.25),
 
+    /**
+     * Represents the normal difficulty level
+     */
     NORMAL(
            "Normal",
            500,
@@ -21,16 +32,9 @@ public enum GameDifficulty {
            1.0f,
            0.35),
 
-    // TESTING(
-    //        "TESTING",
-    //        10000, 
-    //        1.0,
-    //        100.0,
-    //        0.75,
-    //        1.0,
-    //        3.0f,
-    //        0.35),
-
+    /**
+     * Represents the hard difficulty level
+     */
     HARD(
          "Hard",
          400,
@@ -50,6 +54,17 @@ public enum GameDifficulty {
     private final float cartVelocityMultiplier;
     private final double randomEventOdds;
 
+    /**
+     * Constructs a new game difficulty level
+     * @param name The name of the difficulty
+     * @param startingMoney The amount of starting money
+     * @param numberOfCartsMultiplier The number of carts multiplier
+     * @param moneyEarnMultiplier The amount of money earn multiplier
+     * @param sellPriceModifier The sell price modifier
+     * @param towerReloadMultiplier The tower reload multiplier
+     * @param cartVelocityMultiplier The cart velocity multiplier
+     * @param randomEventOdds The odds in which a random event will occur
+     */
     GameDifficulty(
                    String name,
                    int startingMoney,
@@ -70,18 +85,25 @@ public enum GameDifficulty {
         this.randomEventOdds = randomEventOdds;
     }
 
+    /**
+     * Retrieves the name of difficulty level
+     * @return The name of the difficulty
+     */
     public String getName() {
         return name;
     }
 
     /**
-     * @return The amount of money the player starts the game with
+     * Retrieves the amount of money a player starts the game with
+     * @return The amount of starting money
      */
     public int getStartingMoney() {
         return startingMoney;
     }
 
     /**
+     * Calculates the number of carts to spawn given a round nunber
+     * @param roundNumber The number of round
      * @return The number of carts to spawn.
      */
     public int getNumberOfCarts(int roundNumber) {
@@ -91,31 +113,43 @@ public enum GameDifficulty {
     }
 
     /**
-     * @return The mulitplier used to calculate the amount of money earned per round. Formula: money += (roundNumber * moneyEarnMultiplier)
+     * Retrieves the money earn multiplier which affects the amount of money earned per round
+     * This value can be used like money += (roundNumber * moneyEarnMultiplier)
+     * @return The money earn multiplier
      */
     public double getMoneyEarnMultiplier() {
         return moneyEarnMultiplier;
     }
 
     /**
-     * @return The sell back price of items in the shop is calculated by dividing the cost price by this number.
+     * Retrieves the sell price modifier which affects the price an item can be sold for
+     * @return The sell price modifier
      */
     public double getSellPriceModifier() {
         return sellPriceModifier;
     }
 
     /**
-     * @return The number that each tower's reload speed will be divided by.
-     *         Increasing this value will result in towers reloading quicker.
+     * Retrieves the tower reload modifier which affects how fast a tower reloads.
+     * Increasing this value will result in towers reloading quicker.
+     * @return The tower reload modifier
      */
     public double getTowerReloadModifier() {
         return towerReloadModifier;
     }
 
+    /**
+     * Retrieves the cart velocity modifier which affects the velocity of the carts
+     * @return The cart velocity multiplier
+     */
     public float getCartVelocityMultiplier() {
         return cartVelocityMultiplier;
     }
 
+    /**
+     * Retrieves the odds that a random event will occur.
+     * @return The odds a random event will occur in the range [0, 1]
+     */
     public double getRandomEventOdds() {
         return randomEventOdds;
     }
